caf419de48f0042be84730496529766b7e58b91d
Mr.Wang <63017582+TTVMrWang@users.noreply.github.com>
2020-06-17 16:10:00 -0700

    Create solution.py

---
+++ Throwing <N> Amount of Darts Find All Possible Combinations to Reach a Target Score/solution.py
@@ -0,0 +1,5 @@
+from itertools import combinations_with_replacement as cwr
+
+def darts_solver(sections, darts, target):
+	results = [i for i in cwr(sections, darts) if sum(i) == target]
+	return ['-'.join(map(str, i)) for i in results]
