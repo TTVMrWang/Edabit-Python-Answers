f0e44634cc3da2bb2cee7ade9fb2a48304ddfb80
Mr.Wang <63017582+TTVMrWang@users.noreply.github.com>
2020-06-17 22:14:29 -0700

    Create solution.py

---
+++ Edabit's Encryption Scheme/solution.py
@@ -0,0 +1,6 @@
+from math import ceil
+
+def encryption(txt):
+    txt = ''.join(i for i in txt if not i.isspace())
+    root = len(txt)**0.5
+    return ' '.join(txt[i::ceil(root)] for i in range(int(root) + 1))
